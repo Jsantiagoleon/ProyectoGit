57193985f997a288cb3ff3e70f7495059df7b764
Jsantiagoleon <jslp0808@gmail.com>
2024-09-10 09:35:45 -0500

    Esta es la modificacion de santi

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main(){
 
     cout<<"Hola Daniel"<<endl;
-
-
     cout<<"estos son cambios de Daniel"<<endl;
+    int a=4;
+
+     cout<<"estos son cambios de Julian"<<endl;
 
     return 0;
 }
