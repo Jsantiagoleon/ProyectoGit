9a210b72e15490af98375d9628d1716334b4581f
Jsantiagoleon <jslp0808@gmail.com>
2024-09-10 09:45:06 -0500

    Cambio con Santiago

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -9,7 +9,7 @@ int main(){
     cout<<"estos son cambios de Daniel"<<endl;
     int a=4;
 
-     cout<<"estos son cambios de Julian"<<endl;
+     cout<<"estos son cambios de Santiago"<<endl;
 
     return 0;
 }
